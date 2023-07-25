ab1324d8244a690244a270382ed83c0049919eb2
Archana Khaiyarguli <133819245+Archana-sth@users.noreply.github.com>
2023-07-25 17:30:05 -0400

    Update fizzbuzz.py
    FizzBuzz challange.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-# add your code here
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FIZZBUZZ")
+    elif number % 3 == 0:
+        print("FIZZ")
+    elif number % 5 == 0:
+        print("BUZZ")
+    else:
+        print(number)
 
